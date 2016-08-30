8a873a5295d33229707b0dc76209365a01576590
Lovecross <345841634@qq.com>
2016-08-30 14:06:32 +0800

    init

---
+++ IOSUtil/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSUtil
+//
+//  Created by huipay on 2016/8/30.
+//  Copyright © 2016年 huipay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSUtil/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSUtil
+//
+//  Created by huipay on 2016/8/30.
+//  Copyright © 2016年 huipay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
